b2fbb6a2e0b755af771cf9e99669053854afef0b
José A. Romero L <escherdragon@gmail.com>
2016-04-25 01:44:23 +0200

    Bugfix: Added hashCode() to Role

--- src/net/jarl/kata/currencyzoo/model/Role.java
+++ src/net/jarl/kata/currencyzoo/model/Role.java
@@ -10,10 +10,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
 @Entity
 @Table(name="Roles", indexes={
@@ -21,6 +18,7 @@ import lombok.ToString;
 })
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode
 @ToString(exclude={"user"})
 public class Role implements Serializable {
     private static final long serialVersionUID = 1L;
