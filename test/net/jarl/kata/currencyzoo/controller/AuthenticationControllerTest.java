4e12b54f726851787b085bc67757e6aa613e4554
José Alfredo Romero L <escherdragon@gmail.com>
2016-04-29 11:37:35 +0200

    Added tests for AuthenticationController

---
+++ test/net/jarl/kata/currencyzoo/controller/AuthenticationControllerTest.java
@@ -0,0 +1,134 @@
+package net.jarl.kata.currencyzoo.controller;
+
+import static net.jarl.kata.currencyzoo.view.Views.ACCESS_DENIED;
+import static net.jarl.kata.currencyzoo.view.Views.LOGGED_OUT;
+import static net.jarl.kata.currencyzoo.view.Views.LOGIN;
+import static net.jarl.kata.currencyzoo.view.Views.LOGOUT;
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+import mockit.Mocked;
+import mockit.NonStrictExpectations;
+import mockit.Tested;
+import mockit.Verifications;
+import mockit.integration.junit4.JMockit;
+import net.jarl.kata.currencyzoo.configuration.MVCConfig;
+
+/**
+ * Unit tests for the {@link AuthenticationController}
+ *
+ * @author José A. Romero L.
+ */
+@RunWith(JMockit.class)
+@ContextConfiguration(classes={MVCConfig.class})
+public class AuthenticationControllerTest {
+
+    @Tested
+    private AuthenticationController controller;
+
+    @Mocked
+    private SecurityContext securityCtx;
+
+    @Mocked
+    private Authentication authentication;
+
+    @Mocked
+    private UserDetails userDetails;
+
+    @Before
+    public void setUp() {
+        new NonStrictExpectations( SecurityContextHolder.class ) {{
+            SecurityContextHolder.getContext(); result = securityCtx;
+        }};
+        new NonStrictExpectations() {{
+            securityCtx.getAuthentication(); result = authentication;
+            authentication.getPrincipal(); result = userDetails;
+        }};
+    }
+
+    @Test
+    public void loginViewIsDisplayedOnRequest()
+        throws Exception
+    {
+        // when, then:
+        mvc().perform( get( LOGIN ) ).
+            andExpect( status().isOk() ).
+            andExpect( view().name( LOGIN ) );
+    }
+
+    @Test
+    public void accessDeniedIsDisplayedOnRequest()
+        throws Exception
+    {
+        // given:
+        final String username = "zooplus";
+        new NonStrictExpectations() {{
+            userDetails.getUsername(); result = username;
+        }};
+
+        // when, then:
+        mvc().perform( get( ACCESS_DENIED ) ).
+            andExpect( status().isOk() ).
+            andExpect( view().name( ACCESS_DENIED ) ).
+            andExpect( model().attribute( "user", equalTo( username ) ) );
+    }
+
+    @Test
+    public void logoutRequestLogsOutAndRedirectsToLogin(
+        @Mocked SecurityContextLogoutHandler secHandler )
+        throws Exception
+    {
+        // given:
+        String redirectionUrl = LOGGED_OUT.replaceFirst( "redirect:", "" );
+
+        // when, then:
+        mvc().perform( get( LOGOUT ) ).
+            andExpect( status().isFound() ).
+            andExpect( redirectedUrl( redirectionUrl ) );
+
+        new Verifications() {{
+            secHandler.logout( (HttpServletRequest) any, (HttpServletResponse) any, authentication );
+                times = 1;
+        }};
+    }
+
+    private MockMvc mvc() {
+        return MockMvcBuilders.
+            standaloneSetup( new AuthenticationController() ).
+            setViewResolvers( viewResolver() ).
+            build();
+    }
+
+    private ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver =
+            new InternalResourceViewResolver();
+
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+
+        return viewResolver;
+    }
+}
